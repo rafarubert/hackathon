bb0e935ee2cafda8938004e49c4ea0cf4e4aa3f3
Rafael Cruz Rubert <rafarubert@yahoo.com.br>
2012-07-21 10:12:21 -0300

    Adicionado dependencias:
    - Sqlite
    - Removido espaços em branco

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -23,7 +23,7 @@ RSpec.configure do |config|
 
   config.include Devise::TestHelpers, :type => :controller
   config.include Devise::TestHelpers, :type => :helper
-  
+
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
